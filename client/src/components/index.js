04dc1297cdbac02e08c7e11f7beaaaab3e6abeb8
seadadan <siciidcawad@gmail.com>
2021-10-04 10:25:20 +0300

    commit

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,4 +1,4 @@
 export { default as ChannelContainer} from './ChannelContainer';
 export { default as ChannelListContainer} from './ChannellListContainer';
 export { default as ChannelSearch} from './ChannelSearch';
-export { default as Auth} from './Auth';
+export { default as Auth} from './Auth'; 
